658b82708c4329c1f548e3e6fcf4a0bb3083dc6e
Shawn Lee <dashawn@gmail.com>
2012-03-03 14:01:16 -0800

    Implemented IsRouteUsed and ClearRoutes.

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'route.views.Index'),
-    url(r'^IsRouteUsed/(?P<hash>.+)$', 'route.views.IsRouteUsed'),
+    url(r'^IsRouteUsed/(?P<hash_arg>.+)$', 'route.views.IsRouteUsed'),
     url(r'^ClearRoutes', 'route.views.ClearRoutes'),
 )
