4a6c67e93f01a26e9ab5b06aba38f163e689cc2b
Shawn Lee <dashawn@gmail.com>
2012-03-03 13:33:26 -0800

    Added template directory and updated routing.

--- route/views.py
+++ route/views.py
@@ -1,1 +1,11 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+from route.models import Hash
+
 # Create your views here.
+def Index(request):
+  t = loader.get_template('route/home.html')
+  return HttpResponse(t.render(Context({})))
+
+def IsRouteUsed(request, hash):
+  return HttpResponse("Your Hash is %s" % hash)
