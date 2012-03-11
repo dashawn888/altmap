b487eb19cae2e970aa1c4f75c90eebf06890b1f1
Shawn Lee <dashawn@gmail.com>
2012-03-11 07:01:04 -0700

    Various variable cleanups.

--- route/views.py
+++ route/views.py
@@ -1,3 +1,4 @@
+"""Control the views for route."""
 import json
 
 from django.http import HttpResponse
@@ -5,22 +6,25 @@ from django.template import Context, loader
 from route.models import Hash
 
 # Create your views here.
-def Index(request):
-  t = loader.get_template('route/home.html')
-  return HttpResponse(t.render(Context({})))
+def index(_):
+    """Provide the default view for route."""
+    template = loader.get_template('route/home.html')
+    return HttpResponse(template.render(Context({})))
 
-def IsRouteUsed(request, hash_arg):
+def is_route_used(_, hash_arg):
+    """Return a json object if a route is used or not."""
+    for hash_object in Hash.objects.all():
+        if hash_object.hash == hash_arg:
+            return HttpResponse(
+                json.dumps({"Used": True}), mimetype="application/json")
 
-  for hash_object in Hash.objects.all():
-    if hash_object.hash == hash_arg:
-      return HttpResponse(json.dumps({"Used": True}), mimetype="application/json")
+    Hash(hash=hash_arg).save()
+    return HttpResponse(
+        json.dumps({"Used": False}), mimetype="application/json")
 
-  Hash(hash=hash_arg).save()
-  return HttpResponse(json.dumps({"Used": False}), mimetype="application/json")
+def clear_routes(_):
+    """Clear the routes."""
+    for hash_object in Hash.objects.all():
+        Hash.delete(hash_object)
 
-def ClearRoutes(request):
-
-  for hash_object in Hash.objects.all():
-    Hash.delete(hash_object)
-
-  return HttpResponse("Cleared")
+    return HttpResponse("Cleared")
--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'route.views.Index'),
-    url(r'^IsRouteUsed/(?P<hash_arg>.+)$', 'route.views.IsRouteUsed'),
-    url(r'^ClearRoutes', 'route.views.ClearRoutes'),
+    url(r'^$', 'route.views.index'),
+    url(r'^IsRouteUsed/(?P<hash_arg>.+)$', 'route.views.is_route_used'),
+    url(r'^ClearRoutes', 'route.views.clear_routes'),
 )
